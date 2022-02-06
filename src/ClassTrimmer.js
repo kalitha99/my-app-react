9a1fb412ce2f4c178ee613a8102ba8072a322999
Kalitha <kalithadissanayake44@gmail.com>
2022-02-06 13:34:55 +0530

    1

---
+++ src/ClassTrimmer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class ClassTrimmer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {  
+            timer:0
+        }
+    }
+
+    componentDidMount(){
+        this.
+    }
+    
+    render() { 
+        return ( 
+            <div>
+
+            </div>
+         );
+    }
+}
+ 
+export default ClassTrimmer;
