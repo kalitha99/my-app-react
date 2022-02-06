9e3c957a0fca72a171cd9e30f733d67231d861ce
Kalitha <kalithadissanayake44@gmail.com>
2022-02-06 13:35:04 +0530

    23

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import ClassCounter from './components/ClassCounter';
-import ClassCounterOne from './components/ClassCounterOne';
-import HookCounter from './components/HookCounter';
-import HookCounterTwo from './components/HookCounterTwo';
-import HookCounterThree from './components/HookCounterThree';
 import './App.css';
-import HookCounterFour from './components/HookCounterFour';
-import HookCounterFive from './components/HookCounterFive';
+import FocusInput from './focusInput';
+
 
 function App() {
   return (
 <div className="App">
-     {/*<ClassCounter/>*/}
-     {/*<HookCounter/>*/}
-     {/*<HookCounterTwo/>*/}
-     {/*<HookCounterThree/>*/}
-     {/*<ClassCounterOne/>*/}
-     <HookCounterFive/>
+ 
+     <FocusInput/>
     </div>
   );
 }
